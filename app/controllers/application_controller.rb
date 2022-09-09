627ac2701114478a02ba4ecd10f35d5c0268555e
John Kamau <johnjkkamau@gmail.com>
2022-09-09 13:20:41 +0300

    Patch request created in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,42 @@ class ApplicationController < Sinatra::Base
     recipe.destroy_all
     dessert.to_json
   end
+  patch "/desserts/:id" do
+    dessert = Dessert.find(params[:id])
+    dessert.update(
+      name: params[:name],
+      image: params[:image],
+      proteins: params[:proteins],
+      carbohydrates: params[:carbohydrates],
+      fat: params[:fat],
+    )
+    new_ingredients = params[:ingredients]
+    dessert.ingredients.each do |ingredient|
+      if (!new_ingredients.include?(ingredient))
+        ingredient_entry = Ingredient.find_by(name: ingredient)
+        Recipe.where("dessert_id = ? AND ingredient_id = ?", dessert.id, ingredient_entry.id).delete_all
+      end
+    end
+    new_ingredients.each do |new_ingredient|
+      ingredient = Ingredient.find_or_create_by(name: new_ingredient)
+      if Recipe.where("dessert_id = ? AND ingredient_id = ?", dessert.id, ingredient.id).length == 0
+        Recipe.create(dessert_id: dessert.id, ingredient_id: ingredient.id)
+      end
+    end
 
-
+    new_regimes = params[:regimes]
+    dessert.regimes.each do |regime|
+      if (!new_regimes.include?(regime))
+        regime_entry = Regime.find_by(name: regime)
+        Dessert_Regime.where("dessert_id = ? AND regime_id = ?", dessert.id, regime_entry.id).delete_all
+      end
+    end
+    new_regimes.each do |new_regime|
+      regime = Regime.find_or_create_by(name: new_regime)
+      if Dessert_Regime.where("dessert_id = ? AND regime_id = ?", dessert.id, regime.id).length == 0
+        Dessert_Regime.create(dessert_id: dessert.id, regime_id: regime.id)
+      end
+    end
+    dessert.to_json
+  end
 end

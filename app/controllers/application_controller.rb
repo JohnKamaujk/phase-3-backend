7cbd4fe9840bec3e54245be7d07c747e2917446b
John Kamau <johnjkkamau@gmail.com>
2022-09-09 12:54:34 +0300

    Created get request in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/desserts" do
+    Dessert.desserts.to_json
+  end
+  get "/desserts/:id" do
+    dessert = Dessert.find(params[:id])
+    dessert.dessert_obj.to_json
+  end
 
 end

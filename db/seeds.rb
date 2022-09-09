aa381a65537ea3a9d662421454858416056e85bb
John Kamau <johnjkkamau@gmail.com>
2022-09-09 12:19:52 +0300

    Created Regimes & Ingredient instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# Regimes
+Regime.create(name: "low sugar")
+Regime.create(name: "paleo")
+Regime.create(name: "keto")
+Regime.create(name: "vegan")
+Regime.create(name: "gluten free")
+
+Ingredient.create(name: "milk")
+Ingredient.create(name: "almond")
+Ingredient.create(name: "egg")
+Ingredient.create(name: "butter")
+Ingredient.create(name: "chocolate")
+Ingredient.create(name: "sugar")
 
 puts "✅ Done seeding!"
